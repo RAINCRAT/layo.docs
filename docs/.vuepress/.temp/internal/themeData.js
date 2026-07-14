17ca491f19a5c77bfa7b0cf6b4dcdcb6bd3d5202
13812773784 <tc.aibeto@outlook.com>
2026-07-15 02:40:25 +0800

    chore(docs): 清理博客侧边栏旧组件文档配置
    移除了博客侧边栏中冗余的组件文档路由配置，仅保留基础博客列表页路由

--- docs/.vuepress/.temp/internal/themeData.js
+++ docs/.vuepress/.temp/internal/themeData.js
@@ -1,1 +1,14 @@
-export const themeData = JSON.parse("{\"navbar\":[{\"text\":\"文档站首页\",\"link\":\"/\"},{\"text\":\"文档\",\"link\":\"/docs/\"},{\"text\":\"博客\",\"link\":\"/blogs/\"}],\"sidebar\":{\"/docs/\":[\"\",\"style\"],\"/blogs/\":[\"\",\"ak-button\",\"ak-button-group\",\"ak-card\",\"ak-counter\",\"ak-divider\",\"ak-fx\",\"ak-form\",\"ak-icon\",\"ak-level\",\"ak-loading\",\"ak-media\",\"ak-object\",\"ak-panel\",\"ak-pagination\",\"ak-san\"]},\"lastUpdated\":\"上次更新\",\"repo\":\"https://github.com/RAINCRAT/layo.docs\",\"docsDir\":\"docs\",\"editLink\":false,\"editLinkText\":\"帮助改善此页面！( ￣□￣)/\",\"contributors\":false,\"colorMode\":\"dark\",\"locales\":{\"/\":{\"selectLanguageName\":\"English\"}},\"colorModeSwitch\":true,\"logo\":null,\"selectLanguageText\":\"Languages\",\"selectLanguageAriaLabel\":\"Select language\",\"sidebarDepth\":2,\"contributorsText\":\"Contributors\",\"notFound\":[\"There's nothing here.\",\"How did we get here?\",\"That's a Four-Oh-Four.\",\"Looks like we've got some broken links.\"],\"backToHome\":\"Take me home\",\"openInNewWindow\":\"open in new window\",\"toggleColorMode\":\"toggle color mode\",\"toggleSidebar\":\"toggle sidebar\"}")
+export const themeData = JSON.parse("{\"navbar\":[{\"text\":\"文档站首页\",\"link\":\"/\"},{\"text\":\"文档\",\"link\":\"/docs/\"},{\"text\":\"博客\",\"link\":\"/blogs/\"}],\"sidebar\":{\"/docs/\":[\"\",\"style\"],\"/blogs/\":[\"\"]},\"lastUpdated\":\"上次更新\",\"repo\":\"https://github.com/RAINCRAT/layo.docs\",\"docsDir\":\"docs\",\"editLink\":false,\"editLinkText\":\"帮助改善此页面！( ￣□￣)/\",\"contributors\":false,\"colorMode\":\"dark\",\"locales\":{\"/\":{\"selectLanguageName\":\"English\"}},\"colorModeSwitch\":true,\"logo\":null,\"selectLanguageText\":\"Languages\",\"selectLanguageAriaLabel\":\"Select language\",\"sidebarDepth\":2,\"contributorsText\":\"Contributors\",\"notFound\":[\"There's nothing here.\",\"How did we get here?\",\"That's a Four-Oh-Four.\",\"Looks like we've got some broken links.\"],\"backToHome\":\"Take me home\",\"openInNewWindow\":\"open in new window\",\"toggleColorMode\":\"toggle color mode\",\"toggleSidebar\":\"toggle sidebar\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateThemeData) {
+    __VUE_HMR_RUNTIME__.updateThemeData(themeData)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ themeData }) => {
+    __VUE_HMR_RUNTIME__.updateThemeData(themeData)
+  })
+}
